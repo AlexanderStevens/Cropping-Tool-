050e30827a8a01da40395a78bcc2d4fbb5d5cae6
Alex Stevens <alex.stevens@oneiota.co.uk>
2017-09-14 09:59:09 +0100

    Update

--- croppingTool/CroppingViewController.swift
+++ croppingTool/CroppingViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import CoreGraphics
 
 class CroppingViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    
     @IBOutlet weak var holdingViewAspectRatioContraint: NSLayoutConstraint!
     @IBOutlet weak var saveBtn: UIButton!
     var startingPhoto : UIImage?
@@ -44,7 +44,7 @@ class CroppingViewController: UIViewController, UIImagePickerControllerDelegate,
         holdingView.removeConstraint(holdingViewAspectRatioContraint)
         holdingViewAspectRatioContraint = NSLayoutConstraint(item: holdingView, attribute: .width, relatedBy: .equal, toItem: holdingView, attribute: .height, multiplier: ar, constant: 0.0)
         holdingView.addConstraint(holdingViewAspectRatioContraint)
-
+        
     }
     
     private func saveToGallery(croppedImage:UIImage) {
@@ -54,28 +54,28 @@ class CroppingViewController: UIViewController, UIImagePickerControllerDelegate,
     }
     
     @IBAction func savePhoto(_ sender: UIButton) {
-//        let alert = UIAlertController(title: "Select a Photo", message: "Choose a source", preferredStyle: .actionSheet)
-//        let save = UIAlertAction(title: "Save", style: .default, handler: { (UIAlertAction) in
-        let newscale = self.scale(from: self.mainImageView.frame.size.width, to: self.startingPhoto!.size.width)
-        var rect = self.croppingImageView.frame
-        rect.origin.x *= newscale
-        rect.origin.y *= newscale
-        rect.size.width *=  newscale
-        rect.size.height *=  newscale
+        let alert = UIAlertController(title: "Select a Photo", message: "Choose a source", preferredStyle: .actionSheet)
+        let save = UIAlertAction(title: "Save", style: .default, handler: { (UIAlertAction) in
+            let newscale = self.scale(from: self.mainImageView.frame.size.width, to: self.startingPhoto!.size.width)
+            var rect = self.croppingImageView.frame
+            rect.origin.x *= newscale
+            rect.origin.y *= newscale
+            rect.size.width *=  newscale
+            rect.size.height *=  newscale
+            
+            let newImage = self.crop(img: self.mainImageView.image!, to: rect)
+            self.croppingImageView.image = newImage
+            self.saveToGallery(croppedImage: newImage)
+            
+        })
         
-        let newImage = self.crop(img: self.mainImageView.image!, to: rect)
-        self.croppingImageView.image = newImage
-//            self.saveToGallery(croppedImage: newImage)
-//            
-//        })
-//        
-//        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-//        
-//        alert.addAction(save)
-//        alert.addAction(cancel)
-//        
-//        self.present(alert, animated: true, completion: nil)
-
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alert.addAction(save)
+        alert.addAction(cancel)
+        
+        self.present(alert, animated: true, completion: nil)
+        
     }
     @IBAction func changePhoto(_ sender: UIButton) {
         
@@ -108,9 +108,9 @@ class CroppingViewController: UIViewController, UIImagePickerControllerDelegate,
         
         self.present(alert, animated: true, completion: nil)
     }
-
+    
     @IBAction func handlePan(recognizer:UIPanGestureRecognizer) {
-       // print("Holding View Wodth: \(holdingView.frame.size.width)")
+        // print("Holding View Wodth: \(holdingView.frame.size.width)")
         let translation = recognizer.translation(in: self.holdingView)
         if let cropview = recognizer.view {
             let newX = cropview.frame.origin.x + translation.x
@@ -121,12 +121,23 @@ class CroppingViewController: UIViewController, UIImagePickerControllerDelegate,
             let newRect = CGRect(x: newX, y: newY, width: cropview.frame.width, height: cropview.frame.height)
             if holdingView.bounds.contains(newRect){
                 cropview.center = CGPoint(x:cropview.center.x + translation.x, y:cropview.center.y + translation.y)
-                savePhoto(self.saveBtn)
             }
             
         }
         recognizer.setTranslation(CGPoint.zero, in: self.holdingView)
     }
+    
+    @IBAction func handlePinch(recognizer:UIPinchGestureRecognizer) {
+        let view = UIView(frame: croppingImageView.frame)
+        view.transform = view.transform.scaledBy(x: recognizer.scale, y: recognizer.scale)
+        
+        if holdingView.bounds.contains(view.frame) {
+            croppingImageView.frame = view.frame
+        }
+        recognizer.scale = 1
+        
+    }
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let selectedImg = info[UIImagePickerControllerOriginalImage] as? UIImage {
             startingPhoto = selectedImg
@@ -146,13 +157,13 @@ class CroppingViewController: UIViewController, UIImagePickerControllerDelegate,
     
     func crop(img:UIImage, to: CGRect) -> UIImage {
         
-//        print("toRect, x: \(to.origin.x), y:\(to.origin.y), width: \(to.size.width), height: \(to.size.height)")
-//        print(" ")
-//        print("mainImg, width: \(String(describing: img.cgImage?.width)), height: \(String(describing: img.cgImage?.height))")
-//        print("cgImg, width: \(img.size.width), height: \(img.size.height)")
-//        
-//        print("mainImgView, x: \(mainImageView.frame.origin.x), y: \(mainImageView.frame.origin.y), width: \(mainImageView.frame.size.width), height: \(mainImageView.frame.size.height)")
-//        print("Rect, x: \(rect.origin.x), y: \(rect.origin.y), width: \(rect.size.width), height: \(rect.size.height)")
+        //        print("toRect, x: \(to.origin.x), y:\(to.origin.y), width: \(to.size.width), height: \(to.size.height)")
+        //        print(" ")
+        //        print("mainImg, width: \(String(describing: img.cgImage?.width)), height: \(String(describing: img.cgImage?.height))")
+        //        print("cgImg, width: \(img.size.width), height: \(img.size.height)")
+        //
+        //        print("mainImgView, x: \(mainImageView.frame.origin.x), y: \(mainImageView.frame.origin.y), width: \(mainImageView.frame.size.width), height: \(mainImageView.frame.size.height)")
+        //        print("Rect, x: \(rect.origin.x), y: \(rect.origin.y), width: \(rect.size.width), height: \(rect.size.height)")
         
         // Manually get CGImageRef from UIImage ignoring imageOrientation...
         UIGraphicsBeginImageContext(img.size)
